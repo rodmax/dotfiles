b803ad6905ff5c6813b7c78ef7ac9be6d1f3fde1
max.rodionov <max.rodionov@synesis.ru>
2018-10-16 22:23:01 +0300

    vscode: implement deletion of extensions not presented in extensions list

--- bin/code-install-extensions.sh
+++ bin/code-install-extensions.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+extensionsList=$HOME/.config/Code/User/extensions-list.txt
 set -ev
+
+# Install new / Update installed
 for ext in $(cat ~/.config/Code/User/extensions-list.txt); do code --install-extension ${ext}; done
 
+
+# Delete if not found in extensions list
+currentlyInstalled=/tmp/code-extensions-list.txt
+code --list-extensions > ${currentlyInstalled}
+needToDelete=$(comm -23 ${currentlyInstalled} ${extensionsList})
+
+for ext in $needToDelete; do
+    read -p "Delete '$ext' (y/n) extension? " choice
+    case "$choice" in
+    y|Y ) code --uninstall-extension ${ext};;
+    n|N ) echo "skipped";;
+    * ) ;;
+    esac
+done;
