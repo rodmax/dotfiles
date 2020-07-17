ad750d4a5fcddfe6e6cd82395de0911f89121aad
max.rodionov <rodionov.m.m@gmail.com>
2020-07-17 12:01:42 +0300

    setup script: change paths to flameshot binary

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -105,7 +105,7 @@ run_4() {
     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
         name 'flameshot'
     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
-        command '/usr/bin/flameshot gui'
+        command 'flameshot gui'
     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
         binding 'Print'
 }
