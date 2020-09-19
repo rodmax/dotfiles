da5685c3bf0f4e443351c969b5ecbe8cc154ed51
max.rodionov <rodmax@users.noreply.github.com>
2020-09-19 23:36:23 +0300

    setup.sh: remove redundant apt install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,9 +6,7 @@ run_0() {
     sudo add-apt-repository ppa:jonathonf/vim
     sudo apt update
 
-    sudo apt install terminator tmux git zsh tree gitg gawk vim libappindicator-dev flameshot
-    sudo apt install terminator tmux git zsh tree shutter gitg gawk vim shutter libappindicator-dev flameshot
-    sudo apt install htop terminator tmux git zsh tree gitg gawk vim shutter libappindicator-dev flameshot
+    sudo apt install terminator tmux git zsh tree gitg gawk vim libappindicator-dev flameshot htop
 
     mkdir -p ~/bin
     cd ~/bin
